f2997725b30b225e8e35a345679fd670e0a02718
David Franca <daviddsf@fab.mil.br>
2019-07-04 11:37:04 -0300

    Correção de uns erros de path na hora de procurar as classes ja carregadas. Web service corrigido e ticket de criação da anotação jsonreturn, para retornar um json quando o metodo for anotado.

---
+++ devframework/src/main/java/devframework/annotations/JsonReturn.java
@@ -0,0 +1,9 @@
+package devframework.annotations;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JsonReturn {
+
+	
+}	
--- devframework/src/main/java/devframework/webservice/MapeamentoInitializer.java
+++ devframework/src/main/java/devframework/webservice/MapeamentoInitializer.java
@@ -1,20 +1,23 @@
 package devframework.webservice;
 
-/*
- * import org.springframework.web.servlet.support.
- * AbstractAnnotationConfigDispatcherServletInitializer;
- * 
- * public class MapeamentoInitializer extends
- * AbstractAnnotationConfigDispatcherServletInitializer {
- * 
- * @Override protected Class<?>[] getRootConfigClasses() {
- * 
- * return new Class[] { MapeamentoConfiguration.class }; }
- * 
- * @Override protected Class<?>[] getServletConfigClasses() { return null; }
- * 
- * @Override protected String[] getServletMappings() { return new String[] { "/"
- * }; }
- * 
- * }
- */
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MapeamentoInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return new Class[] { MapeamentoConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
