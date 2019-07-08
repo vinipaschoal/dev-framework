568ce7b0baeaa2f9f30a35a5b708fdbf8e61bbf5
David Franca <daviddsf@fab.mil.br>
2019-07-08 14:56:05 -0300

    todos os testes alterados para usar separador padrao

--- devframework/src/test/java/devframework/op/uploadfile/PersistFileTest.java
+++ devframework/src/test/java/devframework/op/uploadfile/PersistFileTest.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
@@ -23,10 +22,13 @@ public class PersistFileTest {
 
 	private PersistFile persistFile;
 
+	private final String diretorioRaiz = System.getProperty("user.dir") + File.separator + "diretorio";
+
 	@Before
 	public void setUp() throws IOException {
 		persistFile = new PersistFile();
-		File diretorio = new File(System.getProperty("user.dir")+File.separator+"diretorio");
+		Utils.getInstance().setProperty("upload.dir", diretorioRaiz);
+		File diretorio = new File(diretorioRaiz);
 		if (diretorio.exists()) {
 			FileUtils.deleteDirectory(diretorio);
 		}
@@ -41,87 +43,72 @@ public class PersistFileTest {
 
 	@Test
 	public void testeListComUploadDirVazio() throws FileNotFoundException {
-		File diretorio = new File("/diretorio");
+		File diretorio = new File(File.separator + diretorioRaiz);
 		diretorio.mkdir();
-		Utils.getInstance().setProperty("upload.dir", "/diretorio");
-		List<String> arquivos = persistFile.list();
-		Assert.assertEquals(0, arquivos.size());
+		Assert.assertEquals(0, persistFile.list().size());
 	}
 
 	@Test
 	public void testeListComUploadDirComDoisArquivosClass() throws IOException {
-		File diretorio = new File("/diretorio");
+		File diretorio = new File(File.separator + diretorioRaiz);
 		diretorio.mkdir();
-		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator
-				+ "resources" + File.separator + "devframework" + File.separator + "domain" + File.separator
-				+ "Agenda.class"), Paths.get("/diretorio/Agenda.class"));
-		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator
-				+ "resources" + File.separator + "devframework" + File.separator + "domain" + File.separator
-				+ "Tarefa.class"),
-				Paths.get("/diretorio/Tarefa.class"));
-		Utils.getInstance().setProperty("upload.dir", "/diretorio");
-		List<String> arquivos = persistFile.list();
-		Assert.assertEquals(2, arquivos.size());
+		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test"
+				+ File.separator + "resources" + File.separator + "devframework" + File.separator + "domain"
+				+ File.separator + "Agenda.class"), Paths.get(diretorioRaiz + File.separator + "Agenda.class"));
+		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test"
+				+ File.separator + "resources" + File.separator + "devframework" + File.separator + "domain"
+				+ File.separator + "Tarefa.class"), Paths.get(diretorioRaiz + File.separator + "Tarefa.class"));
+		Assert.assertEquals(2, persistFile.list().size());
 	}
 
 	@Test
 	public void testeListComUploadDirComUmArquivoClassEOutroGenerico() throws IOException {
-		File diretorio = new File("/diretorio");
+		File diretorio = new File(File.separator + diretorioRaiz);
 		diretorio.mkdir();
-		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator
-				+ "resources" + File.separator + "devframework" + File.separator + "domain" + File.separator
-				+ "Agenda.class"), Paths.get("/diretorio/Agenda.class"));
-		File arquivo2 = new File("/diretorio/arquivo1");
+		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test"
+				+ File.separator + "resources" + File.separator + "devframework" + File.separator + "domain"
+				+ File.separator + "Agenda.class"), Paths.get(diretorioRaiz + File.separator + "Agenda.class"));
+		File arquivo2 = new File(File.separator + diretorioRaiz + File.separator + "arquivo1");
 		arquivo2.createNewFile();
-		Utils.getInstance().setProperty("upload.dir", "/diretorio");
-		List<String> arquivos = persistFile.list();
-		Assert.assertEquals(1, arquivos.size());
+		Assert.assertEquals(1, persistFile.list().size());
 	}
 
 	@Test
 	public void testeListComUploadDirComDoisArquivosGenericos() throws IOException {
-		File diretorio = new File("/diretorio");
+		File diretorio = new File(File.separator + diretorioRaiz);
 		diretorio.mkdir();
-		File arquivo1 = new File("/diretorio/arquivo1");
-		File arquivo2 = new File("/diretorio/arquivo1");
+		File arquivo1 = new File(File.separator + diretorioRaiz + File.separator + "arquivo1");
+		File arquivo2 = new File(File.separator + diretorioRaiz + File.separator + "arquivo2");
 		arquivo1.createNewFile();
 		arquivo2.createNewFile();
-		Utils.getInstance().setProperty("upload.dir", "/diretorio");
-		List<String> arquivos = persistFile.list();
-		Assert.assertEquals(0, arquivos.size());
+		Assert.assertEquals(0, persistFile.list().size());
 	}
 
 	@Test
 	public void testeListComUploadDirComDoisDiretoriosVazios() throws FileNotFoundException {
-		File diretorio = new File(File.separator+System.getProperty("user.dir") + File.separator +"diretorio"+File.separator+"subdiretorio");
+		File diretorio = new File(File.separator + diretorioRaiz + File.separator + "subdiretorio");
 		diretorio.mkdirs();
-		Utils.getInstance().setProperty("upload.dir", System.getProperty("user.dir") + File.separator +"diretorio");
-		List<String> arquivos = persistFile.list();
-		Assert.assertEquals(0, arquivos.size());
+		Assert.assertEquals(0, persistFile.list().size());
 	}
 
 	@Test
 	public void testeListComUploadDirComArquivoClassNoSubdiretorio() throws IOException {
-		File diretorio = new File(File.separator+System.getProperty("user.dir") + File.separator +"diretorio"+File.separator+"subdiretorio");
+		File diretorio = new File(File.separator + diretorioRaiz + File.separator + "subdiretorio");
 		diretorio.mkdirs();
-		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator
-				+ "resources" + File.separator + "devframework" + File.separator + "domain" + File.separator
-				+ "Agenda.class"), Paths.get(System.getProperty("user.dir")+ File.separator +"diretorio"+File.separator+"Agenda.class"));
-		Utils.getInstance().setProperty("upload.dir", System.getProperty("user.dir") + File.separator +"diretorio");
-		List<String> arquivos = persistFile.list();
-		Assert.assertEquals(1, arquivos.size());
+		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test"
+				+ File.separator + "resources" + File.separator + "devframework" + File.separator + "domain"
+				+ File.separator + "Agenda.class"), Paths.get(diretorioRaiz + File.separator + "Agenda.class"));
+		Assert.assertEquals(1, persistFile.list().size());
 	}
 
 	@Test
 	public void testeListComUploadDirComClassSemAnotacaoNecessaria() throws IOException {
-		File diretorio = new File("/diretorio/subdiretorio");
+		File diretorio = new File(File.separator + diretorioRaiz + File.separator + "subdiretorio");
 		diretorio.mkdirs();
-		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator
-				+ "resources" + File.separator + "devframework" + File.separator + "domain" + File.separator
-				+ "Pessoa.class"), Paths.get("/diretorio/Pessoa.class"));
-		Utils.getInstance().setProperty("upload.dir", "/diretorio");
-		List<String> arquivos = persistFile.list();
-		Assert.assertEquals(0, arquivos.size());
+		Files.copy(Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator + "test"
+				+ File.separator + "resources" + File.separator + "devframework" + File.separator + "domain"
+				+ File.separator + "Pessoa.class"), Paths.get(diretorioRaiz + File.separator + "Pessoa.class"));
+		Assert.assertEquals(0, persistFile.list().size());
 	}
 
 }
